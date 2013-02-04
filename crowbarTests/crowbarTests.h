f62d0c9a08d3382fd385a6c5b3e5add0292fe59b
richo <richo@psych0tik.net>
2013-02-04 23:07:48 +1100

    Add xcode stuff.

---
+++ crowbarTests/crowbarTests.h
@@ -0,0 +1,13 @@
+//
+//  crowbarTests.h
+//  crowbarTests
+//
+//  Created by Richo Healey on 4/02/13.
+//  Copyright (c) 2013 Richo Healey. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface crowbarTests : SenTestCase
+
+@end
---
+++ crowbarTests/crowbarTests.m
@@ -0,0 +1,32 @@
+//
+//  crowbarTests.m
+//  crowbarTests
+//
+//  Created by Richo Healey on 4/02/13.
+//  Copyright (c) 2013 Richo Healey. All rights reserved.
+//
+
+#import "crowbarTests.h"
+
+@implementation crowbarTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in crowbarTests");
+}
+
+@end
